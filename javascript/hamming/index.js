696a7380f18ef8f051fe8dda5f6092ee53a05907
jeremymbolin <jeremy@bolinconsultancy.com>
2017-11-14 17:21:45 -0600

    Calculated Hamming Difference In Python and JavaScript

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -3,5 +3,24 @@
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
 
+class Hamming {
+
+    compute(first_strand, second_strand) {
+
+        let computeResult = 0;
+
+        if (first_strand.length !== second_strand.length)
+            throw('DNA strands must be of equal length.'); // throw if invalid
+
+        for (let i = 0; i < first_strand.length; i++) {
+
+            if (first_strand[i] !== second_strand[i]) // compare equality of strings based on indexed position
+                computeResult += 1; // increment result if the string values are different
+
+        }
+
+        return computeResult;
+    }
+}
 
 module.exports = Hamming;
--- python/hamming/__init__.py
+++ python/hamming/__init__.py
@@ -3,5 +3,14 @@
 # unit tests pass and your code meets all of the conditions.
 #
 
-def compute():
-    pass
+def compute(strand_one, strand_two):
+    """
+    Calculates the 'Hamming Distance' between two string values representing DNA strands.
+    :param strand_one: string value representing the first strand passed to the function
+    :param strand_two: string value representing the second strand passed to the function
+    :return: an integer representing the 'Hamming Distance'
+    """
+    if len(strand_one) != len(strand_two):
+        raise ValueError
+    else:
+        return sum(strand_one[i] != strand_two[i] for i in range(len(strand_one)))
